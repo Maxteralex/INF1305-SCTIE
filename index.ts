8f273a11208de244370c8f40ca28efb98ae6e286
João Verçosa <jpkqvercosa@hotmail.com>
2020-12-05 15:44:59 -0300

    Remove antigos

--- index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { TesteAssetContract } from './teste-asset-contract';
-export { TesteAssetContract } from './teste-asset-contract';
-
-export const contracts: any[] = [ TesteAssetContract ];
